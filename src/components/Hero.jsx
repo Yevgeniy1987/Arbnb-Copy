23583ce01da5e90af26a12971c9bb1fa66bccebb
Eugene <evgeniy.lyashko.87@gmail.com>
2023-08-06 16:33:43 -0500

    create airbnb content
    -added components
    -added stylization
    -added images

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="hero--main main">
+      <img className="hero--img" src="../src/assets/Group-77.png" alt="" />
+      <div className="hero--content main">
+        <h1 className="title--main">Online Experiences</h1>
+        <p className="text--main">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Logo(){
+    return (
+        <nav className="logo--bar">
+            <img className="logo" src="../src/assets/airbnb.png" alt="" />
+        </nav>
+    )
+}
